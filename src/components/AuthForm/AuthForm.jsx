7cc39cfe8a6e7f1cd41985ba780e82c4a526f999
Steckij Nikita <stets119@gmail.com>
2022-11-12 13:25:26 +0300

    show password done

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import styles from './AuthForm.module.scss';
 
 function AuthForm({ type, handleChangeForm }) {
+    const [password, setPassword] = React.useState('');
+    const [showed, setShowed] = React.useState(false);
+
     return (
-        <div className={styles.inputContainer} >
-            {type === 'registration' && <input type="text" placeholder='First name' onChange={(event) => handleChangeForm('firstName', event.target.value)} />}
-            {type === 'registration' && <input type="text" placeholder='Last name' onChange={(event) => handleChangeForm('lastName', event.target.value)} />}
-            <input type="email" placeholder='Email' onChange={(event) => handleChangeForm('email', event.target.value)} />
-            <input type="password" placeholder='Password' onChange={(event) => handleChangeForm('password', event.target.value)} />
+        <div className={styles.formContainer} >
+            {type === 'registration' && <input type="text" className={styles.input} placeholder='First name' onChange={(event) => handleChangeForm('firstName', event.target.value)} />}
+            {type === 'registration' && <input type="text" className={styles.input} placeholder='Last name' onChange={(event) => handleChangeForm('lastName', event.target.value)} />}
+            <input type="email" className={styles.input} placeholder='Email' onChange={(event) => handleChangeForm('email', event.target.value)} />
+            <div className={styles.passwordContainer}>
+                <input type={showed ? "text" : "password"} className={styles.passwordInput} value={password} placeholder='Password' onChange={(event) => { setPassword(event.target.value); handleChangeForm('password', password) }} />
+                {password && <button onClick={() => setShowed(!showed)}>{showed ? 'Hide' : 'Show'}</button>}
+            </div>
         </div>
     )
 };
