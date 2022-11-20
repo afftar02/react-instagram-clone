856602e7bc2345154c15be42891d9edc8835128a
Steckij Nikita <stets119@gmail.com>
2022-11-20 14:26:17 +0300

    Home prepared for posts

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Post.module.scss';
+
+function Post() {
+  return (
+    <div className={styles.post}>Post</div>
+  )
+};
+
+export default Post;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Home.module.scss';
 import Navigation from '../../components/Navigation/Navigation';
+import Post from '../../components/Post/Post';
 
 function Home() {
   return (
     <>
       <Navigation />
       <div className={styles.homeContainer}>
+        <div className={styles.postsBlock}>
+          {[1, 2, 3, 4, 5].map(() => (<Post />))}
+        </div>
+        <div className={styles.infoBlock}>
 
+        </div>
       </div>
     </>
   )
