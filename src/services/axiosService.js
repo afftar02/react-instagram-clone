96efdad23d00e182c71415f75ed399c01aceffad
Steckij Nikita <stets119@gmail.com>
2022-12-03 12:10:45 +0300

    auto refresh done

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -8,39 +8,49 @@ const baseUrl = 'http://localhost:4444/api';
 
 // фабрика создания запросов
 export const request = async ({
-    headers = {},
-    method = 'GET',
-    url,
+  headers = {},
+  method = 'GET',
+  url,
+  data,
+  params,
+}) => {
+  // получили токен
+  const { accessToken, refreshToken } = getSessionFromStorage() || {};
+
+  // если есть токен то добавили его в header
+  if (accessToken) {
+    headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  // формируем параметры запроса
+  const options = {
+    headers,
+    method,
     data,
     params,
-  }) => {
-    // получили токен
-    const { accessToken } = getSessionFromStorage() || {};
-  
-    // если есть токен то добавили его в header
-    if (accessToken) {
-      headers.Authorization = `Bearer ${accessToken}`;
-    }
-  
-    // формируем параметры запроса
-    const options = {
-      headers,
-      method,
-      data,
-      params,
-      url: baseUrl + url,
-    };
-  
-    try {
-      // выполняем запрос
-      const result = await axiosInstance(options);
-  
-      return result;
-    } catch (error) {
-      console.error(error);
-  
-      throw error;
-    }
+    url: baseUrl + url,
   };
 
-  
+  try {
+    // выполняем запрос
+    const result = await axiosInstance(options);
+
+    return result;
+  } catch (error) {
+    if (error.response.status === 401 && refreshToken) {
+      options.headers.Authorization = `Bearer ${refreshToken}`;
+      options.method = 'POST';
+      options.url = baseUrl + '/auth/refresh';
+
+      const refreshResponse = await axiosInstance(options);
+      localStorage.setItem('tokens', JSON.stringify({ accessToken: refreshResponse.data.accessToken, refreshToken: refreshToken }));
+
+      const finalResponse = await request({ headers, method, url, data, params });
+
+      return finalResponse;
+    }
+
+    throw error;
+  }
+};
+
