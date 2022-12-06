7dec245fc00c43c990bb7134a3f247b741efec90
Steckij Nikita <stets119@gmail.com>
2022-12-06 22:59:25 +0300

    post creation done

--- src/components/PostCreation/PostCreation.jsx
+++ src/components/PostCreation/PostCreation.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import { useAuth } from '../../auth/Auth';
+import { createPost } from '../../services/postService';
 import styles from './PostCreation.module.scss';
 
 function PostCreation() {
+    const [description, setDescription] = useState('');
+
     const { getUserName } = useAuth();
+
+    async function handleCreation() {
+        if (!description) {
+            alert('Description field is required');
+            return;
+        }
+        await createPost(description);
+        setDescription('');
+    }
 
     return (
         <div className={styles.container}>
             <div className={styles.header}>
-                <button>Share</button>
+                <button onClick={handleCreation}>Share</button>
             </div>
             <div className={styles.main}>
                 <div className={styles.userBlock}>
@@ -17,7 +30,7 @@ function PostCreation() {
                     <span>{getUserName()}</span>
                 </div>
                 <div className={styles.inputBlock}>
-                    <textarea placeholder='Write a caption...' />
+                    <textarea placeholder='Write a caption...' value={description} onChange={(event) => setDescription(event.target.value)} />
                 </div>
             </div>
         </div>
--- src/services/postService.js
+++ src/services/postService.js
@@ -9,3 +9,8 @@ export const getPost = async (id) => {
     const { data } = await request({ url: `/post/${id}` });
     return data;
 };
+
+export const createPost = async (description) => {
+    const { data } = await request({ method: 'POST', url: '/post', data: { description } });
+    return data;
+};
